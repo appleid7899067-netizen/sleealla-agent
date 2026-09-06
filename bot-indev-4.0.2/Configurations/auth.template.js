71b07aec13f04aeef5d9e9b82eea22bd629ba555
appleid7899067-netizen <appleid7899067@gmail.com>
2026-09-07 00:22:27 +0700

    Add OpenRouter credential slot to bot auth template

---
+++ bot-indev-4.0.2/Configurations/auth.template.js
@@ -0,0 +1,24 @@
+module.exports = {
+	discord: {
+		clientID: "",
+		clientSecret: "",
+		clientToken: "",
+	},
+	tokens: {
+		discordBotsOrg: "",
+		discordList: "",
+		discordBots: "",
+		giphyAPI: "",
+		googleCSEID: "",
+		googleAPI: "",
+		imgurClientID: "",
+		microsoftTranslation: "",
+		twitchClientID: "",
+		wolframAppID: "",
+		openExchangeRatesKey: "",
+		omdbAPI: "",
+		gistKey: "",
+		openWeatherMap: "",
+		openrouterAPI: "",
+	},
+};
